ef6b2245fbbdf6e7aea5d7df19c9b53228d84415
DJ Cartwright <dj.cartwright.web@gmail.com>
2025-01-26 08:31:41 -0500

    Initial Commit

---
+++ Codehesive/CodehesiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodehesiveApp.swift
+//  Codehesive
+//
+//  Created by David Cartwright on 2025-01-26.
+//
+
+import SwiftUI
+
+@main
+struct CodehesiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
